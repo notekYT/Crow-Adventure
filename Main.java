13818cfe55c24c579709d7b8b417dabe56d95725
notekYT <jacksongithubrepo@gmail.com>
2025-10-18 19:43:20 -0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,41 @@
+package main;
+
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+
+import javax.swing.JFrame;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		JFrame window = new JFrame();
+		window.setAlwaysOnTop(true);
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setUndecorated(true);
+		window.setResizable(false);
+		window.setTitle("Game");
+		
+		gamePanel gamePanel = new gamePanel();
+		window.add(gamePanel);
+		
+		window.pack();
+		
+		gamePanel.setupFullScreen(window);
+		
+		 GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+	        if (gd.isFullScreenSupported()) {
+	            gd.setFullScreenWindow(window);
+	        } else {
+	            window.setExtendedState(JFrame.MAXIMIZED_BOTH);
+	        }
+		window.setVisible(true);
+		
+		gamePanel.setupGame();
+		gamePanel.startGameThread();
+		
+		
+		
+	}
+
+}
